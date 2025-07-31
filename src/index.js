49d45f6a53de8df3c032e39bc93a25f1580c54e7
Ami0480 <amifuku80@gmail.com>
2025-07-31 15:18:23 +0800

    Added js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-let city = "Perth";
-let apiKey = "25ba4b8ct7fc123o0c3d6fccfc118bbd";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+function searchForCity(event) {
+  event.preventDefault();
 
-function displayTemperature(response) {
-  console.log(response.data);
-  let currentTemperature = document.querySelector("#temperature");
-  let temperature = Math.round(response.data.temperature.current);
-  let city = response.data.city;
-  let description = response.data.condition.description;
-
-  currentTemperature.innerHTML = `The temperature in ${city} is ${temperature}°C, ${description}`;
+  let searchInput = document.querySelector("#search-form-input");
+  let searchCity = document.querySelector("#city");
+  searchCity.innerHTML = searchInput.value;
 }
 
-axios.get(apiUrl).then(displayTemperature);
+let searchElement = document.querySelector("#search-form");
+searchElement.addEventListener("submit", searchForCity);
