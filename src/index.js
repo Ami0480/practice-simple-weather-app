a9a7aa433db55de7fcde6986528d161bd2b28719
Ami0480 <amifuku80@gmail.com>
2025-08-01 10:25:01 +0800

    Added api city and temperature js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
+function refreshWeather(response) {
+  let displayTemperature = document.querySelector("#temperature");
+  let temperature = response.data.main.temp;
+  let searchCity = document.querySelector("#city");
+
+  searchCity.innerHTML = response.data.name;
+  displayTemperature.innerHTML = Math.round(temperature);
+}
+
+function displayCity(city) {
+  let apiKey = "d1193959d2d841ec7555416d715716a6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function searchForCity(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-form-input");
-  let searchCity = document.querySelector("#city");
-  searchCity.innerHTML = searchInput.value;
+
+  displayCity(searchInput.value);
 }
 
 let searchElement = document.querySelector("#search-form");
 searchElement.addEventListener("submit", searchForCity);
+
+displayCity("Perth");
