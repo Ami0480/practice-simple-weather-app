f69ee4f0e77f206c18f59e23d9e2600da5190a68
Ami0480 <amifuku80@gmail.com>
2025-08-01 13:21:07 +0800

    Added more weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,45 @@ function refreshWeather(response) {
   let displayTemperature = document.querySelector("#temperature");
   let temperature = response.data.main.temp;
   let searchCity = document.querySelector("#city");
+  let searchDescription = document.querySelector("#description");
+  let searchHumidity = document.querySelector("#humidity");
+  let searchWind = document.querySelector("#wind");
+  let searchTime = document.querySelector("#time");
+  let dt = response.data.dt;
+  let timezone = response.data.timezone;
+
+  let date = new Date((dt + timezone) * 1000);
 
   searchCity.innerHTML = response.data.name;
+  searchTime.innerHTML = formatDate(date);
+  searchDescription.innerHTML = response.data.weather[0].description;
+  searchHumidity.innerHTML = `${response.data.main.humidity}%`;
+  searchWind.innerHTML = `${response.data.wind.speed}km/h`;
   displayTemperature.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getUTCMinutes();
+  let hours = date.getUTCHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getUTCDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayCity(city) {
